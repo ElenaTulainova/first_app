aea8b1dee47efa05c5f8d25b1955f1c3780754a9
Elena Tulainova <eitulainova@valdosta.edu>
2012-02-08 04:40:06 -0500

    Finished times

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,13 @@
 class StoreController < ApplicationController
   def index
     @products = Product.all
+    @count = increment_counter
   end
 
+  def increment_counter
+    if session[:counter].nil?
+      session[:counter] = 0
+    end
+    session[:counter] += 1
+  end
 end
